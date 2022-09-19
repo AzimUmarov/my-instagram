88d310c6602a38d8d03c27aaa41699fe507688e6
Azimjon Umarov <94437438+theazimjon@users.noreply.github.com>
2022-09-19 15:19:56 +0500

    Delete server directory

--- server/src/models/tokenBlockList.js
+++
@@ -1,9 +0,0 @@
-const { Schema, Types, model } = require("mongoose");
-
-const tokenBlockListSchema = new Schema({
-        token: {type: String}},
-    {
-        timestamps: true
-    });
-
-module.exports = model('TokenBlockList', tokenBlockListSchema);
